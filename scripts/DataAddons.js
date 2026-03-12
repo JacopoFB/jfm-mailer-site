854f17342818552d5a0bf3307357c022f8d636c6
JacopoFB <jacopofb03@gmail.com>
2026-03-12 12:48:13 -0600

    work

---
+++ scripts/DataAddons.js
@@ -0,0 +1,18 @@
+const DataAddons = {
+    "buyback": [
+        {
+            "name": "Email Marketing 4 Drops",
+            "desc": "Four text-enhanced email drops to previous sales and unsold leads that reinforce your mail campaign — guaranteed to deliver 50+ additional leads.",
+            "icon": "",
+            "price": 890
+        },
+        {
+            "name": "Purpose-built AI Lead Follow Up",
+            "desc": "Purpose-built AI makes outbound calls to every lead with up to 10 attempts per lead, engaging customers and forwarding all booked appointments to the designated contact at your store.",
+            "icon": "",
+            "price": 695
+        }
+    ]
+};
+
+export default DataAddons;
---
+++ scripts/DataOptions.js
@@ -0,0 +1,34 @@
+const DataOptions = {
+    "buyback": [
+        {
+            "name": "All Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Conquest",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": "",
+            "alternative": {
+                "name": "All Conquest",
+                "desc": "",
+                "icon": "",
+                "price": 0,
+                "tag": ""
+            }
+        }
+    ]
+};
+
+export default DataOptions;
