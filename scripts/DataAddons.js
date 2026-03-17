03fb86d457e6038849ad8e43efd130119d2f8c68
JacopoFB <jacopofb03@gmail.com>
2026-03-17 11:36:12 -0600

    work

--- scripts/DataAddons.js
+++ scripts/DataAddons.js
@@ -6,6 +6,38 @@ const DataAddons = {
             "icon": "",
             "price": 695
         }
+    ],
+    "credit": [
+        {
+            "name": "Purpose-built AI Lead Follow Up",
+            "desc": "Purpose-built AI makes outbound calls to every lead with up to 10 attempts per lead, engaging customers and forwarding all booked appointments to the designated contact at your store.",
+            "icon": "",
+            "price": 695
+        }
+    ],
+    "bankruptcy": [
+        {
+            "name": "Purpose-built AI Lead Follow Up",
+            "desc": "Purpose-built AI makes outbound calls to every lead with up to 10 attempts per lead, engaging customers and forwarding all booked appointments to the designated contact at your store.",
+            "icon": "",
+            "price": 695
+        }
+    ],
+    "saturation": [
+        {
+            "name": "Purpose-built AI Lead Follow Up",
+            "desc": "Purpose-built AI makes outbound calls to every lead with up to 10 attempts per lead, engaging customers and forwarding all booked appointments to the designated contact at your store.",
+            "icon": "",
+            "price": 695
+        }
+    ],
+    "service": [
+        {
+            "name": "Purpose-built AI Lead Follow Up",
+            "desc": "Purpose-built AI makes outbound calls to every lead with up to 10 attempts per lead, engaging customers and forwarding all booked appointments to the designated contact at your store.",
+            "icon": "",
+            "price": 695
+        }
     ]
 };
 
--- scripts/DataOptions.js
+++ scripts/DataOptions.js
@@ -28,6 +28,126 @@ const DataOptions = {
                 "tag": ""
             }
         }
+    ],
+    "credit": [
+        {
+            "name": "All Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Conquest",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": "",
+            "alternative": {
+                "name": "All Conquest",
+                "desc": "",
+                "icon": "",
+                "price": 0,
+                "tag": ""
+            }
+        }
+    ],
+    "bankruptcy": [
+        {
+            "name": "All Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Conquest",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": "",
+            "alternative": {
+                "name": "All Conquest",
+                "desc": "",
+                "icon": "",
+                "price": 0,
+                "tag": ""
+            }
+        }
+    ],
+    "saturation": [
+        {
+            "name": "All Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Conquest",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": "",
+            "alternative": {
+                "name": "All Conquest",
+                "desc": "",
+                "icon": "",
+                "price": 0,
+                "tag": ""
+            }
+        }
+    ],
+    "service": [
+        {
+            "name": "All Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Conquest",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": ""
+        },
+        {
+            "name": "Half Database",
+            "desc": "",
+            "icon": "",
+            "price": 0,
+            "tag": "",
+            "alternative": {
+                "name": "All Conquest",
+                "desc": "",
+                "icon": "",
+                "price": 0,
+                "tag": ""
+            }
+        }
     ]
 };
 
